cfff5b0d4a7abe801db82d53fd29164221f843ff
Michael Kinnas <michaelkinnas@gmail.com>
2024-07-02 09:55:25 +0300

    fixed issue with multiprocessing in threshold.py, massively improving performance

--- threshold.py
+++ threshold.py
@@ -4,19 +4,20 @@ from torch.utils import data
 from utils.score_fns import max_probability, difference, entropy
 from utils.datasets import ImageNetC, CIFAR10C
 from utils.models_lists import imagenet_models, cifar10_models
-from utils.heuristics import heuristic_search
+from utils.heuristics import heuristic_search_process
 from torch import cuda, hub, inference_mode, argmax, tensor
 from torchvision.transforms import Compose, ToTensor, Normalize
 from tqdm.auto import tqdm
 from torchvision.models import get_model
-from threading import Thread
-from argparse import ArgumentParser 
+from argparse import ArgumentParser
+from multiprocessing.pool import Pool
 
 '''
 This script will run the hypermarameter search on the provided validation dataset
 and then calculate the optimal threshold hyperparamter for each combination of 
 score functions with and without the post-check mechanism, for max accuracy.
 '''
+
 def main():
     parser = ArgumentParser()
 
@@ -83,12 +84,12 @@ def main():
         dataloader = data.DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=2)
         
     entropy_norm_factor = entropy(tensor([1/n_classes for _ in range(n_classes)]).unsqueeze(dim=0))
-    
-    
+
     # Run inference on validation dataset for both models
     report = []
+    print("\nGetting predictions from selected models please wait...")
     with inference_mode():
-        for X, y in tqdm(dataloader, desc="Running inference"):
+        for X, y in tqdm(dataloader):
             X = X.to(device)
 
             preds_a = model_a(X)
@@ -105,56 +106,70 @@ def main():
                     'max_prob_b' : max_probability(yb.unsqueeze(dim=0)),
                     'difference_b' : difference(yb.unsqueeze(dim=0)),
                     'entropy_b' : entropy(yb.unsqueeze(dim=0)) / entropy_norm_factor,
-                    'true': yt
+                    'true': yt.item()
                 })
     
     df = pd.DataFrame(report)
 
-    results = {
-        'max_p_acc': [],
-        'max_p_acc_ps': [],
-        'differenc_acc': [],
-        'differenc_acc_ps': [],
-        'entropy_acc': [],
-        'entropy_acc_ps' : [],
-        'max_p_rev_acc': [],
-        'max_p_rev_acc_ps': [],
-        'differenc_rev_acc': [],
-        'differenc_rev_acc_ps': [],
-        'entropy_rev_acc': [],
-        'entropy_rev_acc_ps' : []
-    }
-
-
     threshold_params = np.linspace(0, 1, args.n_threshold_values)
 
-    threads = [
-        Thread(target=heuristic_search, args = (df['true'], df['classification_a'], df['classification_b'], df['max_prob_a'], df['max_prob_b'], threshold_params, False, results, 'max_p_acc', 'max_p_acc_ps')),
-        Thread(target=heuristic_search, args = (df['true'], df['classification_a'], df['classification_b'], df['difference_a'], df['difference_b'], threshold_params, False, results, 'differenc_acc', 'differenc_acc_ps')),
-        Thread(target=heuristic_search, args = (df['true'], df['classification_a'], df['classification_b'], df['entropy_a'], df['entropy_b'], threshold_params, True, results, 'entropy_acc', 'entropy_acc_ps'))
-             ]
+    n_threads = 16
 
-    for thread in threads:
-        thread.start()
+    step = args.n_threshold_values // n_threads
 
-    for thread in threads:
-        thread.join()
+    maxp_splits = []
+    diff_splits = []
+    entropy_splits = []
+    for i in range(n_threads):
+        maxp_splits.append((df['true'], df['classification_a'], df['classification_b'], df['max_prob_a'], df['max_prob_b'], threshold_params[i * step : (i+1) * step], False))
+        diff_splits.append((df['true'], df['classification_a'], df['classification_b'], df['difference_a'], df['difference_b'], threshold_params[i * step : (i+1) * step], False))
+        entropy_splits.append((df['true'], df['classification_a'], df['classification_b'], df['entropy_a'], df['entropy_b'], threshold_params[i * step : (i+1) * step], True))
+
+    maxp_results = []
+    maxp_ps_results = []
+    diff_results = []
+    diff_ps_results = []
+    entropy_results = []
+    entropy_ps_results = []
+
+    # Multi processing    
+    with Pool(n_threads) as pool:
+        print("\nSearching parameter with max probability, please wait...")
+        for ret in pool.starmap(heuristic_search_process, maxp_splits):
+            maxp_results += ret[0]
+            maxp_ps_results += ret[1]
+
+    with Pool(n_threads) as pool:
+        print("Searching parameter with difference, please wait......")
+        for ret in pool.starmap(heuristic_search_process, diff_splits):
+            diff_results += ret[0]
+            diff_ps_results += ret[1]
+    
+    with Pool(n_threads) as pool:
+        print("Searching parameter with entropy, please wait......")
+        for ret in pool.starmap(heuristic_search_process, entropy_splits):
+            entropy_results += ret[0]
+            entropy_ps_results += ret[1]
+   
+
+    df_max_p = pd.DataFrame(maxp_results)
+    df_diff = pd.DataFrame(diff_results)
+    df_entropy = pd.DataFrame(entropy_results).iloc[::-1].reset_index(drop=True) # Reverse order for entropy
+    df_max_p_ps = pd.DataFrame(maxp_ps_results)
+    df_diff_ps = pd.DataFrame(diff_ps_results)
+    df_entropy_ps = pd.DataFrame(entropy_ps_results).iloc[::-1].reset_index(drop=True) # Reverse order for entropy
 
 
-    df_max_p = pd.DataFrame(results['max_p_acc'])
-    df_diff = pd.DataFrame(results['differenc_acc'])
-    df_entropy = pd.DataFrame(results['entropy_acc']).iloc[::-1].reset_index(drop=True) # Reverse order for entropy
-    df_max_p_ps = pd.DataFrame(results['max_p_acc_ps'])
-    df_diff_ps = pd.DataFrame(results['differenc_acc_ps'])
-    df_entropy_ps = pd.DataFrame(results['entropy_acc_ps']).iloc[::-1].reset_index(drop=True) # Reverse order for entropy
-
-    print(f"Found best threshold hyperparameters for {args.model1}, {args.model2}")
-    print(f"Score function: Max Probability, Threshold: {df_max_p.iloc[df_max_p['accuracy'].idxmax()].iat[0]:.4f}, with achieved accuracy of {df_max_p.iloc[df_max_p['accuracy'].idxmax()].iat[1] / len(dataset) * 100:.2f}%")
-    print(f"Score function: Difference, Threshold: {df_diff.iloc[df_diff['accuracy'].idxmax()].iat[0]:.4f}, with achieved accuracy of {df_diff.iloc[df_diff['accuracy'].idxmax()].iat[1] / len(dataset)* 100:.2f}%")
-    print(f"Score function: Entropy, Threshold: {df_entropy.iloc[df_entropy['accuracy'].idxmax()].iat[0]:.4f}, with achieved accuracy of {df_entropy.iloc[df_entropy['accuracy'].idxmax()].iat[1] / len(dataset)* 100:.2f}%")
-    print(f"Score function: Max Probability, Threshold: {df_max_p_ps.iloc[df_max_p_ps['accuracy'].idxmax()].iat[0]:.4f}, with postcheck, with achieved accuracy of {df_max_p_ps.iloc[df_max_p_ps['accuracy'].idxmax()].iat[1] / len(dataset)* 100:.2f}%")
-    print(f"Score function: Difference, Threshold: {df_diff_ps.iloc[df_diff_ps['accuracy'].idxmax()].iat[0]:.4f}, with postcheck, with achieved accuracy of {df_diff_ps.iloc[df_diff_ps['accuracy'].idxmax()].iat[1] / len(dataset)* 100:.2f}%")
-    print(f"Score function: Entropy, Threshold: {df_entropy_ps.iloc[df_entropy_ps['accuracy'].idxmax()].iat[0]:.4f}, with postcheck, with achieved accuracy of {df_entropy_ps.iloc[df_entropy_ps['accuracy'].idxmax()].iat[1] / len(dataset)* 100:.2f}%")
+    print(f"\nFound threshold parameters for {args.model1}, {args.model2}\n")
+    print(f"--------------------------------Without post-check-----------------------------")
+    print(f"Max Probability, Threshold: {df_max_p.iloc[df_max_p['accuracy'].idxmax()].iat[0]:.4f}, with achieved accuracy of {df_max_p.iloc[df_max_p['accuracy'].idxmax()].iat[1] / len(dataset) * 100:.2f}%")
+    print(f"Difference, Threshold: {df_diff.iloc[df_diff['accuracy'].idxmax()].iat[0]:.4f}, with achieved accuracy of {df_diff.iloc[df_diff['accuracy'].idxmax()].iat[1] / len(dataset)* 100:.2f}%")
+    print(f"Entropy, Threshold: {df_entropy.iloc[df_entropy['accuracy'].idxmax()].iat[0]:.4f}, with achieved accuracy of {df_entropy.iloc[df_entropy['accuracy'].idxmax()].iat[1] / len(dataset)* 100:.2f}%")
+    
+    print(f"\n--------------------------------With post-check--------------------------------")
+    print(f"Max Probability, Threshold: {df_max_p_ps.iloc[df_max_p_ps['accuracy'].idxmax()].iat[0]:.4f}, with achieved accuracy of {df_max_p_ps.iloc[df_max_p_ps['accuracy'].idxmax()].iat[1] / len(dataset)* 100:.2f}%")
+    print(f"Difference, Threshold: {df_diff_ps.iloc[df_diff_ps['accuracy'].idxmax()].iat[0]:.4f}, with achieved accuracy of {df_diff_ps.iloc[df_diff_ps['accuracy'].idxmax()].iat[1] / len(dataset)* 100:.2f}%")
+    print(f"Entropy, Threshold: {df_entropy_ps.iloc[df_entropy_ps['accuracy'].idxmax()].iat[0]:.4f}, with achieved accuracy of {df_entropy_ps.iloc[df_entropy_ps['accuracy'].idxmax()].iat[1] / len(dataset)* 100:.2f}%")
 
 
 if __name__ == '__main__':
--- utils/heuristics.py
+++ utils/heuristics.py
@@ -1,74 +1,7 @@
-from tqdm.auto import tqdm
-
-def heuristic_search(trues, preds_a, preds_b, preds_score_a, preds_score_b, param_range, reverse, results, key_1, key_2):
+def heuristic_search_process(trues, preds_a, preds_b, preds_score_a, preds_score_b, param_range, reverse):
     acc = []
     acc_ps = []
-    # threshold_params = np.linspace(0.0001, 1, 1000)
-    if not reverse:
-        for threshold in tqdm(param_range, total=len(param_range)):            
-            max_p_correct = 0
-            max_p_correct_ps = 0
-            max_p_model_b_usage = 0
-            for true, pred_a, pred_b, pred_score_a, pred_score_b in zip(trues, preds_a, preds_b, preds_score_a, preds_score_b):
-                if pred_score_a >= threshold:
-                    max_p_correct += pred_a == true
-                    max_p_correct_ps += pred_a == true
-                else:
-                    max_p_model_b_usage += 1
-                    max_p_correct += pred_b == true
-                    if pred_score_b > pred_score_a:
-                        max_p_correct_ps += pred_b == true
-                    else:
-                        max_p_correct_ps += pred_a == true
 
-            # print( max_p_correct / len(trues) * 100)
-            acc.append({
-                'threshold' : threshold,
-                'accuracy' : max_p_correct,
-                'model_b_usage': max_p_model_b_usage
-            })
-
-            acc_ps.append({
-                'threshold' : threshold,
-                'accuracy' : max_p_correct_ps,
-                'model_b_usage': max_p_model_b_usage,
-            })
-    else:
-        for threshold in tqdm(param_range, total=len(param_range)):
-            max_p_correct = 0
-            max_p_correct_ps = 0
-            max_p_model_b_usage = 0
-            for true, pred_a, pred_b, pred_score_a, pred_score_b in zip(trues, preds_a, preds_b, preds_score_a, preds_score_b):
-                if pred_score_a <= threshold:
-                    max_p_correct += pred_a == true
-                    max_p_correct_ps += pred_a == true
-                else:
-                    max_p_model_b_usage += 1
-                    max_p_correct += pred_b == true
-                    if pred_score_b < pred_score_a:
-                        max_p_correct_ps += pred_b == true
-                    else:
-                        max_p_correct_ps += pred_a == true
-        
-            acc.append({
-                'threshold' : threshold,
-                'accuracy' : max_p_correct,
-                'model_b_usage': max_p_model_b_usage
-            })
-
-            acc_ps.append({
-                'threshold' : threshold,
-                'accuracy' : max_p_correct_ps,
-                'model_b_usage': max_p_model_b_usage,
-            })
-
-    results[key_1] = acc
-    results[key_2] = acc_ps
-
-def heuristic_search_threadpool(trues, preds_a, preds_b, preds_score_a, preds_score_b, param_range, reverse):
-    acc = []
-    acc_ps = []
-    # threshold_params = np.linspace(0.0001, 1, 1000)
     if not reverse:
         for threshold in param_range:            
             max_p_correct = 0
@@ -86,7 +19,6 @@ def heuristic_search_threadpool(trues, preds_a, preds_b, preds_score_a, preds_sc
                     else:
                         max_p_correct_ps += pred_a == true
 
-            # print( max_p_correct)
             acc.append({
                 'threshold' : threshold,
                 'accuracy' : max_p_correct,
